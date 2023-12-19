fa529ad11d3e3b3019ca88e1fae63fbca91bfce5
DilawaizMustafa <iamdilawaizmstafa@gmail.com>
2023-12-19 17:58:33 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home';
+import {BrowserRouter ,Routes ,Route, Link} from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Event from './pages/Event';
+import Donar from './pages/Donar';
+import AdminLogin from './pages/AdminLogin';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Search from './pages/Search';
+import "./pages/style.css"
+// import Accodian from './accordian/Accodian'
+// import Accordian from './accordian/Accodian'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+   
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/event' element={<Event/>}/>
+            <Route path='/' element={<Donar/>}/>
+            <Route path='/' element={<Register/>}/>
+            <Route path='/' element={<AdminLogin/>}/>
+            <Route path='/search' element={<Search/>}/>
+         
+
+
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
